57ea5c95f6cdce1d8f101aba66e5eb30b0184bce
DoanVan <vanbkat14121102@gmail.com>
2021-11-16 14:14:52 +0700

    Bt OOP

---
+++ vandv/src/kethua/Person.java
@@ -0,0 +1,40 @@
+package kethua;
+
+public class Person {
+    public String name;
+    public String sex;
+    public String address;
+    public String dateOfBirth;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
